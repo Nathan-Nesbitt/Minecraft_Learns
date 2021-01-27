928da4ceb330984506733f61c9d5d016f766f26e
kzlecha <kzlecha@gmail.com>
2021-01-26 17:02:39 -0800

    Add setup.py
    This will be used for publishing the minecraft_learns package to PIP

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+    This is the setup file for the PIP minecraft_learns package.
+
+    Written By: Kathryn Lecha
+    Date: 2021-01-26
+"""
+
+from setuptools import setup
+
+setup(
+    name="minecraft_learns",
+    version="0.0.1",
+    description=
+        "Machine Learning library for Minecraft Education AI interactions",
+    url="https://github.com/Nathan-Nesbitt/Minecraft_Learns",
+    author=(
+        "Carlos Rueda Carrasco, Kathryn Lecha, Nathan Nesbitt, " + 
+        "Adrian Morillo Quiroga"),
+    packages=[
+        "minecraft_learns"
+    ],
+    install_requires=[
+        "pandas"
+    ],
+    zip_safe=False
+)

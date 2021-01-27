eff4e50021cc1385251808b676ab7ea5fa79fb03
kzlecha <kzlecha@gmail.com>
2021-01-27 08:12:43 -0800

    Create skeleton Implementable Model Classes

---
+++ src/models/knn.py
@@ -0,0 +1,38 @@
+"""
+    Defines KNN Model used for Classification 
+    
+    Written By: Kathryn Lecha
+    Date: 2021-01-26
+"""
+
+from generic.classification_model import ClassificationModel
+
+
+class KNN(ClassificationModel):
+    """
+    Model class for KNN Classification
+    """
+    def __init__(self, goal, k, pca):
+        super.__init__(self, goal, pca)
+        self.k = k
+
+    def process_data(self, data):
+        
+        pass
+
+    def train(self):
+        """
+        Train the Model
+        """
+        pass
+
+    def predict(self, X):
+        """
+        Predict the response variable y for the input data X
+        ---
+        @param X: a 2D data matrix of n observations and m predictors
+        """
+        pass
+    
+    def evaluate(self):
+        pass
---
+++ src/models/random_forest_regression.py
@@ -0,0 +1,37 @@
+"""
+    Defines Random Forest Model used for Regression 
+    
+    Written By: Kathryn Lecha
+    Date: 2021-01-26
+"""
+
+
+from generic.regression_model import RegressionModel
+
+
+class RandomForestRegressor(RegressionModel):
+    """
+    Model class for Random Forest Regression
+    """
+    def __init__(self, goal, pca=False):
+        super.__init__(self, goal, pca)
+
+    def process_data(self, data):
+        pass
+
+    def train(self):
+        """
+        Train the Model
+        """
+        pass
+
+    def predict(self, X):
+        """
+        Predict the response variable y for the input data X
+        ---
+        @param X: a 2D data matrix of n observations and m predictors
+        """
+        pass
+    
+    def evaluate(self):
+        pass

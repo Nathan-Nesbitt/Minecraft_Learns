dc1c11dfb1369734a783dba8ec2669838369f90f
kzlecha <kzlecha@gmail.com>
2021-02-12 12:23:51 -0800

    rename src

---
+++ minecraft_learns/minecraft_learns.py
@@ -0,0 +1,14 @@
+"""
+    Example of basic workflow on minecraft_learns server. Each function will
+    be triggered by messages that are send by the client. 
+"""
+
+# data = Data("src/foo.json")
+
+# cleaned_data = Preprocess(data, x_column, y_column)
+
+# model = Model(cleaned_data, model_type)
+
+# trained_model = Train(model)
+
+# prediction = Predict(trained_model, new_data)

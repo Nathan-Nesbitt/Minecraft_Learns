a8c7f1c949edac16a9bbb47d2ca07ba050bde1dd
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-12 12:38:06 -0800

    Added Package Publication PYPI

---
+++ minecraft_learns/models/__init__.py
@@ -0,0 +1,15 @@
+"""
+    Init file for the PIP package config.
+
+    Written By: Nathan Nesbitt
+    Date: 2021-02-12
+"""
+
+# from .command import Command
+from .decision_tree import DecisionTree
+from .kmeans import KMeans
+from .knn import KNN
+from .lda import LDA
+from .linear_regression import LinearRegression
+from .random_forest_classifcation import RandomForestClassifier
+from .random_forest_regression import RandomForestRegressor

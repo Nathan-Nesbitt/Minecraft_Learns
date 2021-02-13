8841a9f1a5ae23a7231038cf99070fe6283a6f2e
kzlecha <kzlecha@gmail.com>
2021-02-13 00:01:24 -0800

    add mse to regression model

--- minecraft_learns/models/generic/regression_model.py
+++ minecraft_learns/models/generic/regression_model.py
@@ -7,6 +7,7 @@ Date: 2021-01-26
 
 
 from model import Model
+from numpy import square, subtract
 
 
 class RegressionModel(Model):
@@ -16,4 +17,11 @@ class RegressionModel(Model):
     """
     def __init__(self, pca=False):
         super.__init__(pca)
+
+    def mse(self, X, y):
+        """
+        return the mse of the model
+        """
+        predicted_y = self.predict(X)
+        return square(subtract(y,predicted_y)).mean()
 

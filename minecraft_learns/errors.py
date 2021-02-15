2fb275ede36f51fe8538b2b77279d195c1f979fc
kzlecha <kzlecha@gmail.com>
2021-02-14 17:43:28 -0800

    add errors for lessons and user inputs

--- minecraft_learns/errors.py
+++ minecraft_learns/errors.py
@@ -10,9 +10,8 @@
 class TypeNotFound(Exception):
     """
         Base Exception Class for all issues regarding user inputted types
-        that can be handled in the game. For example if you try to create
-        an event handler or "hook" for a non-existing event, for example
-        "on-bounce" this can be thrown.
+        that can be handled in minecraft_learns. For example if you try to
+        a model that does not exist, for example "qda" this can be thrown.
     """
     def __init__(
         self, entered_type,
@@ -51,6 +50,8 @@ class ModelNotFound(TypeNotFound):
 class InvalidInputUse(ValueError):
     """
         Error class that defines all invalid inputs from the user
+        For example if you try to use a classification model for a regression
+        problem, this can be thrown.
     """
     def __init__(
         self, entered_type,
@@ -85,3 +86,53 @@ class InvalidDataUse(InvalidInputUse):
             "The entered data cannot be used here"
         )
 
+
+class IncorrectFlow(Exception):
+    """
+        Error class that defines all flow errors during user tasks
+        For example if the user tries to predict without fitting, this error
+        can be thrown
+    """
+    def __init__(
+        self, entered_type,
+        message="The method cannot be executed yet"
+    ):
+        self.entered_type = entered_type
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.entered_type} -> {self.message}'
+
+
+class UnProcessedData(IncorrectFlow):
+    """
+        Exception which indicates that the data has not been processed
+    """
+    def __init__(self, entered_type):
+        super().__init__(
+            entered_type,
+            "The data has not been processed yet"
+        )
+
+
+class ModelNotFit(IncorrectFlow):
+    """
+        Exception which indicates that the model has not been fit yet
+    """
+    def __init__(self, entered_type):
+        super().__init__(
+            entered_type,
+            "The model has not been fit yet"
+        )
+
+
+class NoPrediction(IncorrectFlow):
+    """
+        Exception which indicates that there is no prediction to execute
+    """
+    def __init__(self, entered_type):
+        super().__init__(
+            entered_type,
+            "There is no prediction to execute"
+        )

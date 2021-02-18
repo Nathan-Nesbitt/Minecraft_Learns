6cc1ac33b3b8d1017fcb4a6f7f510e0276773cce
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-17 16:30:15 -0800

    Updated Version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="minecraft_learns",
-    version="0.0.5",
+    version="0.0.6",
     description="Machine Learning library for Minecraft Education AI interactions",
     url="https://github.com/Nathan-Nesbitt/Minecraft_Learns",
     author=(

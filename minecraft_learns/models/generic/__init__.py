b8172d7cfc1ef6f4fded08a62e643c9b38dad044
kzlecha <kzlecha@gmail.com>
2021-02-17 16:49:30 -0800

    add init for general

---
+++ minecraft_learns/models/generic/__init__.py
@@ -0,0 +1,7 @@
+"""
+Import Generic Models
+"""
+
+from minecraft_learns.models.generic.model import Model
+from minecraft_learns.models.generic.classification_model import ClassificationModel
+from minecraft_learns.models.generic.regression_model import RegressionModel

07bdb03ea7bfeacd4cf0eb40831d58af90b820e8
kzlecha <kzlecha@gmail.com>
2021-02-23 12:06:41 -0800

    new version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="minecraft_learns",
-    version="0.0.12",
+    version="0.0.13",
     description="Machine Learning library for Minecraft Education AI interactions",
     url="https://github.com/Nathan-Nesbitt/Minecraft_Learns",
     author=(
